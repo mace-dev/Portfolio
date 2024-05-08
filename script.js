da1ccaf35b78b07b66f63b63a420fca68fe12f5d
Matt Mason <117158120+mace-dev@users.noreply.github.com>
2024-05-08 13:16:02 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+/*MENU DROP DOWN */
+let menuOpen = true; // Set the initial state to open
+
+function toggleMenu() {
+  const menu = document.getElementById("menu");
+  const closedIcon = document.getElementById("closed-icon");
+  const openIcon = document.getElementById("open-icon");
+
+  if (menuOpen) {
+    menu.style.display = "none"; // Hide the menu
+    closedIcon.style.display = "inline"; // Show the closed icon
+    openIcon.style.display = "none"; // Hide the open icon
+    menuOpen = false; // Update the state to closed
+  } else {
+    menu.style.display = "block"; // Show the menu
+    closedIcon.style.display = "none"; // Hide the closed icon
+    openIcon.style.display = "inline"; // Show the open icon
+    menuOpen = true; // Update the state to open
+  }
+}
+
+// Call toggleMenu() initially to open the menu on page load
+toggleMenu();
+
+// Function to toggle the visibility of the "About Me" details section
+function toggleAboutMe() {
+    var aboutMeDetails = document.getElementById("aboutMeDetails");
+    aboutMeDetails.classList.toggle("show");
+}
+
+// Event listener for the "About Me" button
+document.getElementById("explorePortfolioButton").addEventListener("click", function() {
+    toggleAboutMe();
+});
